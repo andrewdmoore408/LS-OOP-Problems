3d4a6f7ee03ca803c0c76615490519bc2d2f3c01
Andrew Moore <andrewdmoore84@gmail.com>
2021-05-12 16:10:06 -0400

    Add OOP Basics: Classes and Objects 1 exercises

---
+++ OOP_basics_classes_and_objects_1/create_the_class.rb
@@ -0,0 +1,11 @@
+=begin
+Create the Class
+
+Create an empty class named Cat.
+
+# NOTES: class definitions begin with the class keyword and end with end (like method definitions). CamelCase is used for class names instead of snake_case
+=end
+
+class Cat
+
+end
